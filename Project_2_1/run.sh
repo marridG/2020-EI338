ca8eea0d92b94dd8a1b15f5eb52830672011e186
marridG <1687450542@qq.com>
2020-11-19 00:17:09 +0800

    update run.sh: remove input test

--- Project_2_1/run.sh
+++ Project_2_1/run.sh
@@ -7,5 +7,3 @@ echo "Entering simple_shell"
 ./simple_shell
 
 # we have now entered the simple_shell, input commands
-# [DEBUG] input test, separated by: (1) SPACE, (2) 2 SPACEs, (3) \t
-adhi chi  --hdiwi 123
