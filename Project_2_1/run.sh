f43869c3c8fce78d691009c474625f7e63d4c0c0
marridG <1687450542@qq.com>
2020-11-19 00:15:37 +0800

    update run.sh: input test

--- Project_2_1/run.sh
+++ Project_2_1/run.sh
@@ -5,3 +5,7 @@ echo "Make-File Done"
 echo ""
 echo "Entering simple_shell"
 ./simple_shell
+
+# we have now entered the simple_shell, input commands
+# [DEBUG] input test, separated by: (1) SPACE, (2) 2 SPACEs, (3) \t
+adhi chi  --hdiwi 123
