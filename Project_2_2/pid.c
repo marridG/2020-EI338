e665350b3046c6e127e681eab7b2f3cb0d502205
marridG <1687450542@qq.com>
2020-11-22 20:17:29 +0800

    reformats

--- Project_2_2/pid.c
+++ Project_2_2/pid.c
@@ -28,26 +28,25 @@ static ssize_t proc_write(struct file *file, const char __user *usr_buf, size_t 
 static struct file_operations proc_ops = {
         .owner = THIS_MODULE,
         .read = proc_read,
+        .write = proc write,
 };
 
 /* This function is called when the module is loaded. */
-static int proc_init(void)
-{
-        // creates the /proc/procfs entry
-        proc_create(PROC_NAME, 0666, NULL, &proc_ops);
+static int proc_init(void) {
+    // creates the /proc/procfs entry
+    proc_create(PROC_NAME, 0666, NULL, &proc_ops);
 
-        printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
+    printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
 
-	return 0;
+    return 0;
 }
 
 /* This function is called when the module is removed. */
-static void proc_exit(void) 
-{
-        // removes the /proc/procfs entry
-        remove_proc_entry(PROC_NAME, NULL);
+static void proc_exit(void) {
+    // removes the /proc/procfs entry
+    remove_proc_entry(PROC_NAME, NULL);
 
-        printk( KERN_INFO "/proc/%s removed\n", PROC_NAME);
+    printk(KERN_INFO "/proc/%s removed\n", PROC_NAME);
 }
 
 /**
@@ -58,63 +57,65 @@ static void proc_exit(void)
  * once it has collected the data that is to go into the 
  * corresponding /proc file.
  */
-static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
-{
-        int rv = 0;
-        char buffer[BUFFER_SIZE];
-        static int completed = 0;
-        struct task_struct *tsk = NULL;
+static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos) {
+    int rv = 0;
+    char buffer[BUFFER_SIZE];
+    static int completed = 0;
+    struct task_struct *tsk = NULL;
 
-        if (completed) {
-                completed = 0;
-                return 0;
-        }
+    if (completed) {
+        completed = 0;
+        return 0;
+    }
 
-        tsk = pid_task(find_vpid(l_pid), PIDTYPE_PID);
+    tsk = pid_task(find_vpid(l_pid), PIDTYPE_PID);
 
-        completed = 1;
+    completed = 1;
 
-        // copies the contents of kernel buffer to userspace usr_buf 
-        if (copy_to_user(usr_buf, buffer, rv)) {
-                rv = -1;
-        }
+    // copies the contents of kernel buffer to userspace usr_buf
+    if (
+    copy_to_user(usr_buf, buffer, rv
+    )) {
+    rv = -1;
+    }
 
-        return rv;
+    return
+    rv;
 }
 
 /**
  * This function is called each time we write to the /proc file system.
  */
-static ssize_t proc_write(struct file *file, const char __user *usr_buf, size_t count, loff_t *pos)
-{
-        char *k_mem;
+static ssize_t proc_write(struct file *file, const char __user *usr_buf, size_t count, loff_t *pos) {
+    char *k_mem;
 
-        // allocate kernel memory
-        k_mem = kmalloc(count, GFP_KERNEL);
+    // allocate kernel memory
+    k_mem = kmalloc(count, GFP_KERNEL);
 
-        /* copies user space usr_buf to kernel buffer */
-        if (copy_from_user(k_mem, usr_buf, count)) {
-		printk( KERN_INFO "Error copying from user\n");
-                return -1;
-        }
+    /* copies user space usr_buf to kernel buffer */
+    if (copy_from_user(k_mem, usr_buf, count)) {
+        printk( KERN_INFO
+        "Error copying from user\n");
+        return -1;
+    }
 
-	/**
- 	 * kstrol() will not work because the strings are not guaranteed
-	 * to be null-terminated.
-	 * 
-	 * sscanf() must be used instead.
-	 */
+    /**
+      * kstrol() will not work because the strings are not guaranteed
+     * to be null-terminated.
+     *
+     * sscanf() must be used instead.
+     */
 
-        kfree(k_mem);
+    kfree(k_mem);
 
-        return count;
+    return count;
 }
 
 /* Macros for registering module entry and exit points. */
-module_init( proc_init );
-module_exit( proc_exit );
+module_init(proc_init);
+module_exit(proc_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Module");
-MODULE_AUTHOR("SGG");
+MODULE_DESCRIPTION("Module Modified upon SGG");
+MODULE_AUTHOR("G");
 
