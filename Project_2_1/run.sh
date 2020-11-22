c1a9c37dd2540ba9f46a04184bcca1816095b3e0
marridG <1687450542@qq.com>
2020-11-22 12:37:21 +0800

    update run.sh: add echo commands

--- Project_2_1/run.sh
+++ Project_2_1/run.sh
@@ -7,17 +7,26 @@ echo "Entering simple_shell"
 
 echo "Pre-Designed Commands Set"
 ./simple_shell <<'EOF'
+echo !!
 !!
-echo "DEBUG=False"
+echo DEBUG=False
+echo --config debug false
 --config debug false
+echo !!
 !!
 
+echo ls
 ls
+echo ls > out.txt
 ls > out.txt
+echo cat < out.txt
 cat < out.txt
 
+echo ls | sort
 ls | sort
+echo cat < out.txt | sort > out_sorted.txt
 cat < out.txt | sort > out_sorted.txt
+echo cat out_sorted.txt
 cat out_sorted.txt
 
 
@@ -31,8 +40,11 @@ cat < out.txt
 ls | sort
 cat < out.txt | sort > out_sorted.txt
 
+--config debug false
 echo "Try Unrecognizable Commands, We can also Exit Shell by Calling \"exit\" Once"
+echo 123456
 123456
+echo 123 456 789
 123 456 789
 
 exit
