3ed3b142aa92cb506aa0404ca4d33c95fbfbf52a
marridG <1687450542@qq.com>
2020-11-22 12:19:24 +0800

    update run.sh

--- Project_2_1/run.sh
+++ Project_2_1/run.sh
@@ -4,8 +4,44 @@ echo "Make-File Done"
 
 echo ""
 echo "Entering simple_shell"
-./simple_shell <<'EOF'
 
-# we have now entered the simple_shell, input commands
+echo "Pre-Designed Commands Set"
+./simple_shell <<'EOF'
+echo "DEBUG = False"
+!!
+--config debug false
+!!
+
 ls
+ls > out.txt
+cat < out.txt
+
+ls | sort
+cat < out.txt | sort > out_sorted.txt
+cat out_sorted.txt
+
+
+echo "DEBUG = True"
+!!
+--config debug true
+!!
+
+ls
+ls > out.txt
+cat < out.txt
+
+ls | sort
+cat < out.txt | sort > out_sorted.txt
+cat out_sorted.txt
+
+
+echo "Try Unrecognizable Commands, We can also Exit Shell by Calling \"exit\" Once"
+123456
+123 456 789
+
+exit
 EOF
+
+echo ""
+echo "Now Try Your Commands"
+./simple_shell
