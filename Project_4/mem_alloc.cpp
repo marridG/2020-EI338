720a8188ac165c8fc7efeb25a495974a54d19c43
marridG <1687450542@qq.com>
2020-12-08 21:07:31 +0800

    finish request_memory functionalities

--- Project_4/mem_alloc.cpp
+++ Project_4/mem_alloc.cpp
@@ -6,19 +6,22 @@ using namespace std;
 #define DEBUG
 
 
-struct memory_block {
-    size_t start = -1, end = -1;
+struct MEMORY_BLOCK {
+    size_t start = -1, end = -1, size = -1;
+    int used = -1;
     char *label;
-    struct memory_block *prev, *next;
+    struct MEMORY_BLOCK *prev, *next;
 };
-size_t memory_size = 0;
-memory_block *memory_all;
+size_t MEMORY_SIZE = 0;
+MEMORY_BLOCK *MEMORY_ALL;
+const int MAX_LABEL_LEN = 10;
 
 
-memory_block *create_new_block(size_t start, size_t end, const char *label,
-                               memory_block *prev, memory_block *next);
+MEMORY_BLOCK *create_new_block(size_t start, size_t end, const char *label, int used,
+                               MEMORY_BLOCK *prev, MEMORY_BLOCK *next);
 int init(int argc, char *argv[]);
 void output_values();
+MEMORY_BLOCK *find_best_hole(size_t size, char mode);
 int request_memory(const char *label, size_t size, char mode);
 int release_resources(const char *label);
 int compact();
@@ -41,13 +44,13 @@ int main(int argc, char *argv[]) {
     int op_success = -1;                        // operation succeeds: 0=×,1=√,-1=?
     while (1 == scanf("%s", op)) {
         op_success = -1;
-        char label[10];                         // operand label
+        char label[MAX_LABEL_LEN];                         // operand label
         size_t size = -1;                       // operand target memory size
-        char mode[2];                           // operand resources allocation request mode
+        char mode;                           // operand resources allocation request mode
 
         if (strcmp(op, "RQ") == 0) {            // request resources
             scanf("%s %zu %c", label, &size, &mode);
-            // op_success = (0 == request_resources(customer_idx, request));
+            op_success = (0 == request_memory(label, size, mode));
         }
 
         else if (strcmp(op, "RL") == 0) {       // release resources
@@ -91,78 +94,48 @@ int init(int argc, char *argv[]) {
         return -1;
     }
 
-    sscanf(argv[1], "%zu", &memory_size);
-    memory_all = create_new_block(0, memory_size - 1, "", NULL, NULL);
+    sscanf(argv[1], "%zu", &MEMORY_SIZE);
+    MEMORY_ALL = create_new_block(0, MEMORY_SIZE - 1, "", 0, NULL, NULL);
 
-    if (NULL == memory_all) {
-        printf("[Error] Memory Block Initialization Failed.\n");
-        return -2;
-    }
-    printf("[INFO] Initialized %zu Bytes.\n", memory_size);
+    printf("[INFO] Initialized %zu Bytes.\n", MEMORY_SIZE);
     return 0;
 }
 
 
 int request_memory(const char *label, size_t size, char mode) {
-    //     if (customer_idx < 0 || customer_idx > NUM_CUSTOMERS - 1) {
-    //         printf("[Error] Invalid Customer Index. ");
-    //         return -1;
-    //     }
-    //     if (0 == is_leq(request, need[customer_idx], NUM_RESOURCES)) {
-    //         printf("[Error] Maximum Claim Exceeded. ");
-    //         return -2;
-    //     }
-    //     if (0 == is_leq(request, available, NUM_RESOURCES)) {
-    //         printf("[Error] Insufficient Available Resources. ");
-    //         return -3;
-    //     }
-    //
-    // #ifdef DEBUG
-    //     output_values();
-    // #endif
-    //     // try to allocate
-    //     for (int r = 0; r <= NUM_RESOURCES - 1; r++) {
-    //         available[r] -= request[r];
-    //         allocation[customer_idx][r] += request[r];
-    //         need[customer_idx][r] -= request[r];
-    //     }
-    //     int success = (1 == state_is_safe());
-    //     if (1 == success) { return 0; }
-    //     // if unsafe, restore the unsafe allocation
-    // #ifdef DEBUG
-    //     output_values();
-    // #endif
-    //     for (int r = 0; r <= NUM_RESOURCES - 1; r++) {
-    //         available[r] += request[r];
-    //         allocation[customer_idx][r] -= request[r];
-    //         need[customer_idx][r] += request[r];
-    //     }
-    //     printf("[Error] Allocation Unsafe. ");
-    //     return -4;
+    MEMORY_BLOCK *hole = find_best_hole(size, mode);
+    if (NULL == hole) {
+        printf("[Error] No Available Memory Holes.\n");
+        return -1;
+    }
+
+    hole->label = new char[sizeof(char) * (strlen(label) + 1)];
+    strcpy(hole->label, label);
+
+    // split the remaining memory if exists
+    if (size == hole->size) return 0;
+    hole->next = create_new_block(hole->start + size, hole->end, "", 0, hole, hole->next);
+    hole->end = hole->start + size - 1;
+    hole->size = size;
+    return 0;
 }
 
 int release_resources(const char *label) {
-    //     if (customer_idx < 0 || customer_idx > NUM_CUSTOMERS - 1) {
-    //         printf("[Error] Invalid Customer Index. ");
-    //         return -1;
-    //     }
-    //     if (0 == is_leq(release, allocation[customer_idx], NUM_RESOURCES)) {
-    //         printf("[Error] Alloaction Exceeded. ");
-    //         return -2;
-    //     }
-    //
-    // #ifdef DEBUG
-    //     output_values();
-    // #endif
-    //     // release
-    //     for (int r = 0; r <= NUM_RESOURCES - 1; r++) {
-    //         available[r] += release[r];
-    //         allocation[customer_idx][r] -= release[r];
-    //     }
-    // #ifdef DEBUG
-    //     output_values();
-    // #endif
-    //
+    MEMORY_BLOCK *crt_block = MEMORY_ALL;
+    int target_found = 0;
+    while (NULL != crt_block) {
+        if (0 == target_found && 0 != strcmp(crt_block->label, label)) {
+            crt_block = crt_block->next;
+            continue;
+        }
+        target_found = 1;
+        delete[] crt_block->label;
+        crt_block->label = NULL;
+        // merge with possible unused previous block
+        if (NULL != crt_block->prev && NULL == crt_block->prev->label) {
+            // todo
+        }
+    }
     //     return 0;
 }
 
@@ -180,25 +153,31 @@ int compact() {
  * @param next              pointer of the next memory block
  * @return                  newly created memory block
  */
-memory_block *create_new_block(size_t start, size_t end, const char *label,
-                               memory_block *prev, memory_block *next) {
-    memory_block *new_block = new memory_block;
+MEMORY_BLOCK *create_new_block(size_t start, size_t end, const char *label, int used,
+                               MEMORY_BLOCK *prev, MEMORY_BLOCK *next) {
+    MEMORY_BLOCK *new_block = new MEMORY_BLOCK;
     if (NULL == new_block) {
         printf("[Error] Failed to Create New Memory Block Representation.\n");
         exit(-1);
     }
 
     // assign values of the features of the new memory block
-    // start & end indices in the memory block
     new_block->start = start;
     new_block->end = end;
-    // (un)used block' label
-    if (0 == strlen(label)) {         // unused block
+    new_block->size = end - start + 1;
+    if (0 == strlen(label) && 0 == used) {         // unused block
         new_block->label = NULL;
+        new_block->used = 0;
     }
-    else {                          // used block
+    else if (0 != strlen(label) && 1 == used) {         // used block
         new_block->label = new char[sizeof(char) * (strlen(label) + 1)];
         strcpy(new_block->label, label);
+        new_block->used = 1;
+    }
+    else {
+        delete new_block;
+        printf("[Error] Used & StrLen Mismatch.\n");
+        exit(-2);
     }
     // insert and link the new block
     new_block->prev = prev;
@@ -209,6 +188,41 @@ memory_block *create_new_block(size_t start, size_t end, const char *label,
     return new_block;
 }
 
+MEMORY_BLOCK *find_best_hole(size_t size, char mode) {
+    MEMORY_BLOCK *crt_block = MEMORY_ALL;
+    MEMORY_BLOCK *best_block = NULL;    // pointer to the best block under various modes
+    size_t min_size = MEMORY_SIZE;   // size of the best block under BEST mode
+    size_t max_size = 0;   // size of the best block under WORST mode
+    while (NULL != crt_block) {
+        if (0 != crt_block->used || crt_block->size < size) continue;
+
+        switch (mode) {
+            case 'F':   // first fit
+                return crt_block;
+            case 'B': {   // best fit
+                if (crt_block->size < min_size) {
+                    best_block = crt_block;
+                }
+                break;
+            }
+            case 'W': {// worst fit
+                if (crt_block->size > max_size) {
+                    best_block = crt_block;
+                }
+                break;
+            }
+            default: {
+                printf("[Error] Unknown Mode %c.\n", mode);
+                exit(-3);
+            }
+        }
+        crt_block = crt_block->next;
+    }
+
+    return best_block;
+}
+
+
 /*!
  * Output Values: Memory Holes Usage Status
  */
@@ -218,7 +232,7 @@ void output_values() {
     printf("Memory Report\n");
     printf("==============================\n");
 
-    memory_block *crt_block = memory_all;
+    MEMORY_BLOCK *crt_block = MEMORY_ALL;
     while (NULL != crt_block) {
         printf("%sAddress(es) [%07zu - %07zu]  =>  ",
                indent.c_str(), crt_block->start, crt_block->end);
